58d65e8a0b3f53a19f69df9b9411271341f3f48e
AdAstraAbyssoque <AUGENSTERN_SKY@OUTLOOK.COM>
2024-03-16 13:34:02 +0800

    A little bit changes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,27 +1,28 @@
 module.exports = {
-    title: 'AdAstraAbyssosque',
-    description: 'This is my own space to write about things I like and share them with the world.',
-    base: '/',
-    themeConfig: {
-      nav: [
-        { text: '主页', link: '/' },
-        { text: '文章', link: '/articles/' },
+  title: 'AdAstraAbyssosque',
+  description: 'This is my own space to write about things I like and share them with the world.',
+  base: '/',
+  themeConfig: {
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '文章', link: '/articles/' },
+    ],
+    sidebar: {
+      '/articles/': [
+        {
+          text: 'Articles',
+          children: [
+            {
+              text:'是乍见之欢， 也是天各一方',
+              link:'/articles/是乍见之欢， 也是天各一方.html',
+            },
+            {
+              text:'关于我',
+              link:'/articles/关于我.html',
+            }
+          ],
+        },
       ],
-      sidebar: {
-        '/articles/': getArticlesSidebar(),
-      },
     },
   }
-  
-  function getArticlesSidebar() {
-    return [
-      {
-        text: 'Articles',
-        children: [
-          '/articles/是乍见之欢， 也是天各一方.html',
-          '/articles/关于我.html',
-        ],
-      },
-    ]
-  }
-  
+}
