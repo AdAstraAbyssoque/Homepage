a6f5d956e7d9ffcd076e90d2828ba02f74e60f27
HKUSTGZiGem <ustgzigem@outlook.com>
2024-03-24 21:01:25 +0800

    Add feature of View transition.

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -3,13 +3,12 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import Layout from './layouts/Layout.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+    return h(Layout)
   },
   enhanceApp({ app, router, siteData }) {
     // ...
